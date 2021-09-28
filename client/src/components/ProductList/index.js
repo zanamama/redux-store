5426714fa8b2ef6226857be1b8da1638fe1d9791
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-28 16:02:52 -0400

    added imports for usedispatch and the const & state

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,6 +6,13 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
+import { useDispatch, useSelector } from 'react-redux'
+
+function ProductList() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  const { currentCategory } = state
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
